4b911ad23fe26a47ebc7e166fbe6e16008dff1ab
Bob Mercier <mercier@Bobs-MBP.attlocal.net>
2021-12-09 20:13:59 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+gmake OSX_BUILD=1 CPP='gcc-11 -E' \
+    AS=/usr/local//Cellar/x86_64-elf-binutils/2.36.1/x86_64-elf/bin/as \
+    OBJCOPY=/usr/local//Cellar/x86_64-elf-binutils/2.36.1/x86_64-elf/bin/objcopy
